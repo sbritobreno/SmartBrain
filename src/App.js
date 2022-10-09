671929bea5cfa73de59c1adf9a66e1eabb3069f8
Breno Brito <breno.silvabrito@hotmail.com>
2022-10-09 20:41:35 +0100

    Create Rank component and a new backgroung with ParticlesBg

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from'./components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank'
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color="#fafafa" num={200} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
