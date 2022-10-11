3cf648d110679b781a2cb15eee916d0cdf9ce6c5
Breno Brito <breno.silvabrito@hotmail.com>
2022-10-11 09:06:15 +0100

    Create FaceRecognition component and functionallity

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from'./components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank'
-import ParticlesBg from 'particles-bg';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg color="#fafafa" num={200} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+
+
+const app = new Clarifai.App({
+  apiKey: "17fabc0319ef4432a4fc90afb98c8fc6",
+})
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+  
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(
+        function(response){
+          console.log(response);
+        },
+        function(err){
+          console.log(err);
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg color="#fafafa" num={200} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
