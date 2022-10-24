a7d853666d8310fe5a6e5f097d4d429955851d51
Breno Brito <breno.silvabrito@hotmail.com>
2022-10-24 21:39:45 +0100

    Fix bug clean up state

--- src/App.js
+++ src/App.js
@@ -15,23 +15,24 @@ const app = new Clarifai.App({
   apiKey: process.env.REACT_APP_API_KEY,
 })
 
+const initialState ={
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -83,6 +84,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, { entries: count }))
           })
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -91,7 +93,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if(route === 'signout'){
-      this.setState({ isSignedIn: false})
+      this.setState(initialState)
     } else if(route === 'home') {
       this.setState({ isSignedIn: true})
     }
